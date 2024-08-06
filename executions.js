b33ff67de65b534a71a47ab04c0378252436f48a
Merchant151 <joshntram@gmail.com>
2024-08-06 09:10:34 -0400

    got the forcast

--- executions.js
+++ executions.js
@@ -1,3 +1,4 @@
 	//	fetch(url,{method:"get"});
 		fetch("https://api.weather.gov/alerts/active").then((response) => response.json()).then((json) => console.log(json));
-
+	//Forcast 
+		fetch("https://api.weather.gov/points/38.8977,-77.0365").then((response) => response.json()).then((json) => console.log(json)); 
