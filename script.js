835a651519c5df96a176ad96c16dab39ac29e7ae
Merchant151 <jmiller32@ggc.edu>
2024-08-17 14:44:49 -0400

    test string to grab all forcast elements

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 
-function main(){
-document.getElementById('debug').addEventListener("click",function(){console.log('goto Debug');window.location.href='debug.html';});
+async function main()
+{
+	//debug button
+	document.getElementById('debug').addEventListener("click",function(){console.log('goto Debug');window.location.href='debug.html';});
+	
+	//get forcast.
+	//add to index.html
+	let testString = document.getElementById('debugscript');
+	let str = document.createElement('span');
+	str.textContent = 'fooBar';
+	testString.appendChild(str);
+
+
+
 }
 
 main();
