f64c76569cc8ac105795d8a1960cf87b2f2746e7
Merchant151 <joshntram@gmail.com>
2024-08-18 21:27:09 -0400

    grabbed data for main screen

--- script.js
+++ script.js
@@ -8,10 +8,37 @@ async function main()
 	//add to index.html
 	let testString = document.getElementById('debugscript');
 	let str = document.createElement('span');
-	str.textContent = 'fooBar';
+	let weatherData = await weather();
+	let temp = JSON.stringify(weatherData.properties.periods[0].temperature);
+	let unit = JSON.stringify(weatherData.properties.periods[0].temperatureUnit);
+	let shorty = JSON.stringify(weatherData.properties.periods[0].shortForecast);
+	//let temp = JSON.stringify(weatherData.properties.perods[0].temperature);
+	str.textContent = ''+temp+unit+shorty;
 	testString.appendChild(str);
 
 
+
+}
+
+async function weather()
+{
+	///these consts are temporary
+	const cords = "38.8977,-77.0365"
+	const apiStr= "https://api.weather.gov/points/";
+
+	//process needed to build forcast from cords 
+	/*
+	 * weather API can't get a forcast from cords but can return the closest weather station. 
+	 * individual weather staions provide a forcast 
+	 * so first I need to grab the weather station then grab the forcast (or alrady know which weather station I want)
+	 */
+	let stationParent = await fetch(apiStr+cords);
+	let station = await (stationParent.json())
+	let stationURL= await station.properties.forecast
+	let forecast = await fetch(stationURL);
+	let weatherData = await forecast.json();
+	console.log(weatherData);
+	return weatherData;
 
 }
 
